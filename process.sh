d8c12847833235a17c53490f11030593bde904cb
Халиуллин_Ринат <rinat229@edge1.ru-central1.internal>
2021-11-22 23:53:30 +0300

    wwbrrwb

--- process.sh
+++ process.sh
@@ -1,6 +1,6 @@
 cat $1 | awk -F, '
-{if ($18 >=5) x+=$18}
-END{print "RATING_AVG", x/NR}'
+{if ($18 >= 0) x+=$18; if ($18 >= 0) num+=1}
+END{print "RATING_AVG", x/num}'
 v=`cat $1 | awk -F, '{split($1, countries, "_"); countries1 = countries[1]; print countries1}' | sort -u | tr ' ' '\n' | sort -u | tr '\n' ' '`
 
 for i in $v
@@ -12,7 +12,7 @@ countrieswithhilton=`cat $1 | grep -E "hilton" `
 v=`cat $1 | awk -F, '{split($1, countries, "_"); countries1 = countries[1]; print countries1}' | sort -u | tr ' ' '\n' | sort -u | tr '\n' ' '`
 for i in $v
 do
-    holinn=`echo "${countrieswithholinn}" | grep -E ^"${i}" | awk -F, '{ if ($12 >=0 ) x+=$12} END{print x/NR}'`
-    hilton=`echo "${countrieswithhilton}" | grep -E ^"${i}" | awk -F, '{ if ($12 >=0 ) x+=$12} END{print x/NR}'`
+    holinn=`echo "${countrieswithholinn}" | grep -E ^"${i}" | awk -F, '{  x+=$12} END{print x/NR, x}'`
+    hilton=`echo "${countrieswithhilton}" | grep -E ^"${i}" | awk -F, '{  x+=$12} END{print x/NR, x}'`
     printf "CLEANLINESS $i $holinn $hilton \n"
 done
