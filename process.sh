88e17cdaa973299828055701a6f7effaf62a5414
Халиуллин_Ринат <rinat229@edge1.ru-central1.internal>
2021-11-23 00:42:41 +0300

    wwbrrwb

--- process.sh
+++ process.sh
@@ -22,9 +22,9 @@ gnuplot <<- EOF
 set terminal png size 300, 400
 set output 'c_vs_o.png'
 set datafile separator comma
-plot 'hotels_right.csv' using 12:18 title 'clean_vs_overall' with points
+plot 'tmp/hotels_right.csv' using 12:18 title 'clean_vs_overall' with points
 f(x)=m*x+b
-fit f(x) 'hotels.csv' using 12:18 via m,b
+fit f(x) 'tmp/hotels_right.csv' using 12:18 via m,b
 set output 'c_vs_o.png' 
-plot 'hotels_right.csv' using 12:18 title 'cleaafsf' with points, f(x) title 'fit'
+plot 'tmp/hotels_right.csv' using 12:18 title 'cleaafsf' with points, f(x) title 'fit'
 EOF
